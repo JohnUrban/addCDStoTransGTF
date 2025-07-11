beb5caeb61493219abdfcc2178928e6885d38369
JohnUrban <mr.john.urban@gmail.com>
2025-07-11 15:31:43 -0400

    further instruction

--- conda-deps/install.sh
+++ conda-deps/install.sh
@@ -1,4 +1,12 @@
 #
 #conda create -n addCDStoTransGTF-deps bioconda::ucsc-fasize bioconda::borf bioconda::gffread bioconda::gff3sort -y
 
+
 conda create -n addCDStoTransGTF-deps python=3.10.4 bioconda::ucsc-fasize bioconda::borf bioconda::gffread bioconda::gff3sort -y  
+
+
+exit
+
+OR:
+conda env create -f addCDStoTransGTF-deps.linux.yml       
+conda env create -f addCDStoTransGTF-deps.MacOS-M4max.yml
